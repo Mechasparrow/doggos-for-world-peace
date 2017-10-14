c6da8f3f1c16a58bf08d70cc9da039fe0722f151
Haran Kumar <hkumar8200@gmail.com>
2017-10-14 10:05:28 -0500

    Start script

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.send('hello world')
 })
