d58b1f67983142e6422b24489080359330ab6d52
Qubit <mikipux7@gmail.com>
2017-10-14 15:21:17 -0500

    Implemented Pet view

--- frontend/src/pages/loaner-pet-view/loaner-pet-view.ts
+++ frontend/src/pages/loaner-pet-view/loaner-pet-view.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import {Pet} from '../../models/Pet';
+import {Loaner} from '../../models/Loaner';
+
+//Import the DoggoApi
+import {HttpClient} from '@angular/common/http';
+import {DoggosApi} from '../../api/DoggosApi';
+
 /**
  * Generated class for the LoanerPetViewPage page.
  *
@@ -15,7 +22,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoanerPetViewPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public loaner:Loaner;
+  public loaner_pets:Pet[] = [];
+  private doggos_api:DoggosApi;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http_client: HttpClient) {
+
+    this.loaner = <Loaner> navParams.get('loaner');
+    this.doggos_api = new DoggosApi(http_client);
+
+    let that = this;
+
+    this.doggos_api.getPets().then (function (pets) {
+        var loaner_id = that.loaner.loaner_id;
+
+        that.loaner_pets = <Pet[]>(<any>pets).filter( function (item) {
+          return item.owner_id == loaner_id;
+        })
+
+    }).then (function () {
+      console.log(that.loaner_pets);
+    })
+
   }
 
   ionViewDidLoad() {
